de4b283ca653dd5961f41c6009d515b76d4c3e77
Renan Duarte <renan.rodrigo.duarte@gmail.com>
2024-04-01 20:31:07 -0300

    Ex 6 e 7

--- Aula 4/Exercício 5/Pessoa.cs
+++ Aula 4/Exercício 5/Pessoa.cs
@@ -5,5 +5,12 @@
         // Atributos da pessoa
         public string Nome;
         public int Idade;
+
+        // Construtor
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
     }
 }
