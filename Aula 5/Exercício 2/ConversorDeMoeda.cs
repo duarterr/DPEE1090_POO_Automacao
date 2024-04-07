7088e398609e996e1c663fbe1c17971479e5c865
Renan Duarte <renan.rodrigo.duarte@gmail.com>
2024-04-07 11:55:34 -0300

    Ex 2

---
+++ Aula 5/Exercício 2/ConversorDeMoeda.cs
@@ -0,0 +1,14 @@
+﻿namespace Course
+{
+    internal class ConversorDeMoeda
+    {
+        // Valor do Iof
+        public static double Iof = 0.06;
+
+        // Método para converter dolares em reais
+        public static double DolarParaReal(double quantidade, double cotacao)
+        {
+            return (quantidade * cotacao)*(1.0 + Iof);            
+        }
+    }
+}
