7e09690662499244a2d489a1a3e55b5476637faa
Renan Duarte <renan.rodrigo.duarte@gmail.com>
2024-04-07 11:29:08 -0300

    Membro estatico 1

---
+++ Aula 5/Exemplo Membro Estatico 1/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿namespace Course
+{
+    internal class Calculadora
+    {
+        // Valor de Pi - Constante
+        public double Pi = 3.1415;
+
+        // Método para calcular a circunferência dado um raio
+        public double Circunferencia(double raio)
+        {
+            return 2.0*Pi*raio;
+        }
+
+        // Método para calcular o volume de uma esfera dado um raio
+        public double Volume(double raio)
+        {
+            return (4.0/3.0) * Pi * Math.Pow(raio, 3);
+        }
+    }
+}
