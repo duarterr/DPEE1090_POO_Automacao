c6ba7b749b9f3a0d64d56fcd1cf0d493ed705b4a
Renan Duarte <renan.rodrigo.duarte@gmail.com>
2024-04-17 14:18:22 -0300

    Exemplo 2

---
+++ Aula 6/Exemplo 2/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    internal class Conta
+    {
+        public string Titular;
+        public double Saldo;
+
+        // Construtor da classe
+        public Conta(string titular, double saldo)
+        {
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        // Método para efetuar um depósito
+        public void Deposito (double valor)
+        {
+            Saldo += valor;
+        }
+
+        // Método para efetuar um saque
+        public void Saque (double valor)
+        {
+            if (Saldo >= valor) 
+            { 
+                Saldo -= valor;
+            }
+        }
+
+        //  Método para exibir os dados na tela 
+        public override string ToString() 
+        {
+            return Titular + ": Saldo: " + Saldo.ToString("F2");
+        }
+    }
+}
---
+++ Aula 6/Exemplo 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criação de um objeto "conta"
+            Conta conta = new Conta("Astolfo", 0);
+
+            // Exibição dos dados
+            Console.WriteLine(conta);
+
+            // Deposito inicial de 1000 dinheiros
+            conta.Deposito(1000);
+            Console.WriteLine(conta);
+
+            // Tentativa de saque inválida pois não há saldo
+            conta.Saque(20000);
+            Console.WriteLine(conta);
+
+            // Manipulação direta do atributo "saldo" - Objeto perde a consistência pois saldo só poderia ser aumentado através de um depósito
+            conta.Saldo = 100000000;
+
+            // Agora tentativa de saque é valida pois "criou-se" saldo sem um depósito
+            conta.Saque(20000);
+            Console.WriteLine(conta);
+
+        }
+    }
+}
