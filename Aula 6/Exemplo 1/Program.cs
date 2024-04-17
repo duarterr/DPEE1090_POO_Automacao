62f70a37a81ec236c28cffe90e864030616ded09
Renan Duarte <renan.rodrigo.duarte@gmail.com>
2024-04-17 13:48:18 -0300

    Exemplo 1

---
+++ Aula 6/Exemplo 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do raio: ");
+            double Raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("A circunferencia vale: " + Calculadora.Circunferencia1(Raio));
+            Console.WriteLine("A circunferencia vale: " + Calculadora.Circunferencia2(Raio));
+        }
+    }
+}
