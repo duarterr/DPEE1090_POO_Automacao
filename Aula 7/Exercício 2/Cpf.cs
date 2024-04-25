f1d4e0e9d5bf027a06b970e8c377b2259c47579d
Renan Duarte <renan.rodrigo.duarte@gmail.com>
2024-04-25 14:48:10 -0300

    Update

--- Aula 7/Exercício 2/Cpf.cs
+++ Aula 7/Exercício 2/Cpf.cs
@@ -3,45 +3,21 @@
     // Classe para validação de CPF
     internal class Cpf
     {
-        public static bool Valida (string cpf)
+        public static bool Valida(string cpf)
         {
-            int Soma = 0;
-            int Verificador = 0;
-            int Div = 0;
-            int Resto = 0;
-
             // Checa se CPF recebido é nulo ou não tem 11 caracteres
-            if (string.IsNullOrEmpty (cpf) || cpf.Length != 11)
+            if (string.IsNullOrEmpty(cpf) || cpf.Length != 11)
                 return false;
 
-            // Calculo do primeiro verificador
-            for (int i = 0; i < 9; i++) 
-                Soma += (_GetDigito(cpf, i) * (10 - i));
-
-            Div = Soma / 11;
-            Resto = Soma % 11;
-
-            if (Div < 2)
-                Verificador = 0;
-            else
-                Verificador = 11 - Resto;
+            // Calcula verificador 1
+            int Verificador = _CalculaVerificador(cpf, 1);
 
             // Se verificador 1 é diferente do 10 numero do CPF recebido, CPF é inválido
             if (Verificador != _GetDigito(cpf, 9))
                 return false;
-            
+
             // Calculo do segundo verificador
-            Soma = 0;
-            for (int i = 0; i < 10; i++)
-                Soma += (_GetDigito(cpf, i) * (11 - i));
-
-            Div = Soma / 11;
-            Resto = Soma % 11;
-
-            if (Div < 2)
-                Verificador = 0;
-            else
-                Verificador = 11 - Resto;
+            Verificador = _CalculaVerificador(cpf, 2);
 
             // Se verificador é diferente do 11 numero do CPF recebido, CPF é inválido
             if (Verificador != _GetDigito(cpf, 10))
@@ -55,6 +31,25 @@
         private static int _GetDigito(string cpf, int index)
         {
             return int.Parse(cpf[index].ToString());
+        }
+
+        // Calculo dos verificadores (verificador = 1 para primeiro verificador, 2 para segundo)
+        private static int _CalculaVerificador(string cpf, int verificador)
+        {
+            int Soma = 0;
+            int Div = 0;
+            int Resto = 0;
+
+            for (int i = 0; i < (8 + verificador); i++)
+                Soma += (_GetDigito(cpf, i) * (9 + verificador - i));
+
+            Div = Soma / 11;
+            Resto = Soma % 11;
+
+            if (Div < 2)
+                return 0;
+            else
+                return 11 - Resto;
         }
     }
 }
