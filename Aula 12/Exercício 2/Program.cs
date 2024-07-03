b8df7b9474f299e33373baf43506882b2678ad23
Renan Duarte <renan.rodrigo.duarte@gmail.com>
2024-07-03 16:35:42 -0300

    Exercicio2

---
+++ Aula 12/Exercício 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Course
+{
+    internal class Program
+    {
+        // Método principal do programa, ponto de entrada da aplicação
+        static void Main(string[] args)
+        {
+            SistemaMonitoramento sistema = new SistemaMonitoramento();
+            sistema.IniciarMonitoramento();
+        }
+    }
+}
---
+++ Aula 12/Exercício 2/SensorEmergencia.cs
@@ -0,0 +1,17 @@
+﻿namespace Course
+{
+    // Classe para sensores de emergência (herda de Sensor)
+    public class SensorEmergencia : Sensor
+    {
+        public override double Medir()
+        {
+            // Simulação de medição de emergência (valor fixo para exemplo)
+            return 100.0;
+        }
+
+        public override void Monitorar()
+        {
+            Console.WriteLine($"*** ALERTA DE EMERGÊNCIA *** - Medição de sensor {Nome} excedeu o limite!");
+        }
+    }
+}
