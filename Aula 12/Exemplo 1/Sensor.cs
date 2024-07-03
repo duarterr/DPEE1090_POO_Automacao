1d860f5dacfea790d3ba3fb4e9e41186568917c0
Renan Duarte <renan.rodrigo.duarte@gmail.com>
2024-07-03 15:14:18 -0300

    12_1

---
+++ Aula 12/Exemplo 1/Sensor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class Sensor
+    {
+        // Método para ler dados de um sensor de temperatura
+        public double LerDados(int idSensor)
+        {
+            // Simulação de leitura de dados do sensor de temperatura
+            return 25.0; // Exemplo de retorno
+        }
+
+        // Método para ler dados de um sensor de pressão
+        public double LerDados(int idSensor, string unidade)
+        {
+            // Simulação de leitura de dados do sensor de pressão
+            return 101325.0; // Exemplo de retorno
+        }
+
+        // Método para ler dados de um sensor de umidade
+        public double LerDados(int idSensor, string unidade, double precisao)
+        {
+            // Simulação de leitura de dados do sensor de umidade
+            return 60.0; // Exemplo de retorno
+        }
+    }
+}
