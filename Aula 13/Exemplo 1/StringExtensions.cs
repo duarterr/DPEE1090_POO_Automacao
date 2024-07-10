18b23afa65656772ee0f287c7f6d5ed8fa252181
Renan Duarte <renan.rodrigo.duarte@gmail.com>
2024-07-10 14:17:35 -0300

    Ex1

---
+++ Aula 13/Exemplo 1/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace System
+{
+    // Classe estática para conter métodos de extensão
+    public static class StringExtensions
+    {
+        // Método de extensão para contar palavras em uma string
+        public static int ContarPalavras(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) 
+                return 0;
+
+            return 
+                str.Split(' ').Length;
+        }
+    }
+}
