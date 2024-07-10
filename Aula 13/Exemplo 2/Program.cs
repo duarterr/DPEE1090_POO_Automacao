e8b353670303a7797a8089f56e11302f4bd60eb6
Renan Duarte <renan.rodrigo.duarte@gmail.com>
2024-07-10 15:27:07 -0300

    Ex2

---
+++ Aula 13/Exemplo 2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(n1/n2);
+            }
+            catch (DivideByZeroException ex) 
+            {
+                Console.WriteLine("Tentativa de divisão por zero");
+            }
+            catch (FormatException ex) 
+            {
+                Console.WriteLine("Formato invalido");
+            }
+        }
+    }
+}
